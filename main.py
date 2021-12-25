0689757ca91f1cee1ae055c21f8f49a9c6b486ab
Srikar KSV <srikar1awesome@gmail.com>
2021-12-25 18:16:25 +0530

    📦 NEW: Created a simple web server

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from wsgiref import simple_server
+
+
+def app(environ, response):
+    response("200 OK", [("Content-Type", "text/plain")])
+    return [b"Hello world"]
+
+
+if __name__ == "__main__":
+    PORT = os.getenv("PORT", 3000)
+    httpd = simple_server.make_server("", PORT, app)
+    print(f"✅ Listening on http://localhost:{PORT}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Shutting down...")
+        httpd.server_close()
