e3e103b1d96cc35bf66335b6f90efffcbfa0ff42
Srikar KSV <srikar1awesome@gmail.com>
2021-12-25 21:25:43 +0530

    👌 IMPROVE: Static files are served from static folder

--- pyproject/__init__.py
+++ pyproject/__init__.py
@@ -1,6 +1,10 @@
-from pyproject.lib.serve_files import render_template
+from .lib.serve_files import isRequestFile, render_template, serve_static_files
 
 
 def app(environ, response):
+    # Serve static file
+    if isRequestFile(environ):
+        return serve_static_files(environ, response)
+
     response("200 OK", [("Content-Type", "text/html")])
     return render_template("index.html")
--- pyproject/lib/serve_files.py
+++ pyproject/lib/serve_files.py
@@ -1,5 +1,29 @@
+from wsgiref import util
 from jinja2 import Template
 from pathlib import Path
+import mimetypes
+
+
+def isRequestFile(environ):
+    return bool(Path(environ["PATH_INFO"]).suffix)
+
+
+def serve_static_files(environ, response):
+    static_file = (
+        Path.resolve(Path.cwd())
+        / "pyproject"
+        / "static"
+        / environ["PATH_INFO"].lstrip("/")
+    )
+
+    # If requested file does not exist
+    if not Path(static_file).is_file():
+        response("404 Not Found", [("Content-Type", "text/plain")])
+        return [b"404 Not Found"]
+
+    file_type = mimetypes.guess_type(static_file.name)[0]
+    response("200 OK", [("Content-Type", file_type)])
+    return util.FileWrapper(open(static_file, "rb"))
 
 
 def render_template(file):
