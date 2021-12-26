007751da5fcda3459b168bfed681760e75d15b0e
Srikar KSV <srikar1awesome@gmail.com>
2021-12-26 18:23:41 +0530

    🐛 FIX: Changed import path

--- pyproject/main/routes.py
+++ pyproject/main/routes.py
@@ -1,6 +1,8 @@
-from pyproject.lib.serve_files import render_template
+from pyproject.lib import render_template
 
 # Build router for request method and give 403 is wrong
 def router(environ, response):
-    print(environ)
+    URL = environ["PATH_INFO"]
+    HTTP_METHOD = environ["REQUEST_METHOD"]
+
     return render_template("index.html", response, {"title": "Home"})
