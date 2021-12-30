4bd0b9fd7da36546851c7dae0cf30b413c3c8001
Srikar KSV <srikar1awesome@gmail.com>
2021-12-30 14:54:19 +0530

    👌 IMPROVE: Created a modal to confirm delete

--- pyproject/static/js/app.js
+++ pyproject/static/js/app.js
@@ -1,12 +1,33 @@
 const form = document.querySelector(".delete-post");
+const modal = document.querySelector(".modal");
 
-form.addEventListener("submit", handleDelete);
+form.addEventListener("submit", openModal);
+
+modal.addEventListener("click", handleDelete);
 
 function handleDelete(e) {
-  e.preventDefault();
-  shouldSubmit = confirm("Confirm to delete post");
+  // If clicked outise modal, exit
+  const isOutside = !e.target.closest(".modal-inner");
+  if (isOutside) return closeModal();
 
-  if (shouldSubmit) {
-    e.target.submit();
-  }
+  // Choose action for deleting post
+  const element = e.target;
+  if (element.classList.contains("modal-cancel")) return closeModal();
+  if (element.classList.contains("modal-delete")) return form.submit();
 }
+
+function openModal(e) {
+  e.preventDefault();
+  modal.classList.add("open");
+}
+
+function closeModal() {
+  modal.classList.remove("open");
+}
+
+// Press escape to exit modal
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
